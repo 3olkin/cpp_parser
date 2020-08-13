80ddc4d3b90c202d692070fa826a0505ab61f3d1
Z4RX <iamz4rx@gmail.com>
2020-08-13 20:50:38 +0300

    save

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    int i = 0, j, k;
-    printf("%p", &i);
-    printf("\r\n\f\v(!'$#)+,-:;<=>?@^_‘{|}~\n");
-    printf("%c", '"');
-    return 0;
+int main(int argc, char const *argv[]) {
+  int i = 0, j, k;
+  printf("%p", &i);
+  printf(("(!'$#)+,-:;<=>?@^_‘{|}~\n"));
+  printf("%s%s", "qwerty", (("azerty")));
+  return 0;
 }
