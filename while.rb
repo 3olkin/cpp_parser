685a30b0de7cfdd5e3e92c4e45f8a270d141d3b0
Z4RX <iamz4rx@gmail.com>
2020-08-22 01:40:38 +0300

    setovik dz2

--- while.rb
+++ while.rb
@@ -1,45 +1,128 @@
-# устанавливаем относительный путь до файла
-current_path = File.dirname(__FILE__)
-file_path = current_path + '/setovik.cpp'
+begin
+  # устанавливаем относительный путь до файла
+  current_path = File.dirname(__FILE__)
+  file_path = current_path + '/setovik.cpp'
 
-# считываем код из файла
-if File.exist?(file_path)
-  file = File.new(file_path, 'r')
-  lines = file.readlines
-  file.close
-  code = lines.join(' ').delete("\n").downcase.squeeze(' ')
-  p code
+  # считываем код из файла
+  if File.exist?(file_path)
+    file = File.new(file_path, 'r')
+    lines = file.readlines
+    file.close
+    code = lines.join(' ').delete("\n").downcase.squeeze(' ')
+    p code
+  end
+rescue StandardError
+  puts message
 end
 
 def tokenizer(code)
   require 'strscan'
 
-  tokens = []
-  scanner = StringScanner.new code
-  until scanner.empty?
-    if scanner.scan(/\s+/)
-    elsif match = scanner.scan(/while(?=\s|\()/)
-      tokens << ['wh', match]
-    elsif match = scanner.scan(/\(/)
-      tokens << ['(', match]
-    elsif match = scanner.scan(/\)/)
-      tokens << [')', match]
-    elsif match = scanner.scan(/{/)
-      tokens << ['{', match]
-    elsif match = scanner.scan(/}/)
-      tokens << ['}', match]
-    elsif match = scanner.scan(/;/)
-      tokens << [';', match]
-    elsif match = scanner.scan(/=/)
-      tokens << ['=', match]
-    elsif match = scanner.scan(/[[:alpha:]](?:[[:alnum:]]|_)*(?=\W)/)
-      tokens << ['id', match]
-    elsif match = scanner.scan(/(?:\-|\+)?[[:digit:]]+(?=\W)/)
-      tokens << ['cn', match]
+  [].tap do |tokens|
+    scanner = StringScanner.new code
+    until scanner.empty?
+      if scanner.scan(/\s+/)
+      elsif match = scanner.scan(/while(?=\s|\()/)
+        tokens << ['wh', match]
+      elsif match = scanner.scan(/\(/)
+        tokens << ['(', match]
+      elsif match = scanner.scan(/\)/)
+        tokens << [')', match]
+      elsif match = scanner.scan(/{/)
+        tokens << ['{', match]
+      elsif match = scanner.scan(/}/)
+        tokens << ['}', match]
+      elsif match = scanner.scan(/;/)
+        tokens << [';', match]
+      elsif match = scanner.scan(/=/)
+        tokens << ['=', match]
+      elsif match = scanner.scan(/[[:alpha:]](?:[[:alnum:]]|_)*(?=\W)/)
+        tokens << ['id', match]
+      elsif match = scanner.scan(/(?:\-|\+)?[[:digit:]]+(?=\W)/)
+        tokens << ['cn', match]
+      else
+        raise
+      end
     end
-  end
 
-  tokens
+    situations = [
+      ['wh', '('],
+      [';', 'wh'],
+      [')', 'wh'],
+      ['{', 'wh'],
+      [')', '{'],
+      [')', ';'],
+      [';', '}'],
+      ['}', '}']
+    ]
+    i = 0
+    while i < (tokens.length - 1)
+      tokens.insert(i + 1, ['@@', '']) if situations.include?([tokens[i][0], tokens[i + 1][0]])
+      i += 1
+    end
+    tokens.insert(0, ['@@', ''])
+  end
 end
 
-p tokenizer(code)
+def analyzer(tokens)
+  table = automaton_table
+  header = table_header
+  raise 'no while call' unless tokens[1][0] == 'wh'
+
+  [].tap do |arr|
+    stack = []
+    i = 1
+    row = 0
+    while i < tokens.length
+      col = header.index(tokens[i][0])
+      col = 0 if col.nil?
+      case table[row][col]
+      when 1
+        stack.push(i - 1)
+      when 2
+        nil
+      when 3
+        arr << [].tap do |tmp|
+          i0 = stack.pop
+          (i0..i).each do |j|
+            tmp.push(tokens[j]) if tokens[j][0] != '@@'
+          end
+        end
+      when 4
+        raise "error happened in this sequence: #{tokens[i - 3]}#{tokens[i - 2]}#{tokens[i - 1]}#{tokens[i]}"
+      else
+        raise "unknown error happened. check: #{tokens[i - 3]}#{tokens[i - 2]}#{tokens[i - 1]}#{tokens[i]}"
+      end
+
+      row = col
+      i += 2
+    end
+
+    raise 'stack is not empty!' unless stack.empty?
+  end
+end
+
+def automaton_table
+  [
+    [4, 1, 4, 4, 4, 4, 4, 4],
+    [4, 4, 1, 4, 4, 4, 4, 4],
+    [4, 4, 4, 3, 4, 4, 4, 4],
+    [4, 1, 4, 4, 4, 3, 2, 4],
+    [4, 4, 4, 4, 4, 2, 4, 4],
+    [4, 1, 4, 4, 2, 4, 4, 3],
+    [4, 1, 4, 4, 2, 2, 4, 4],
+    [4, 4, 4, 4, 4, 4, 4, 3]
+  ]
+end
+
+def table_header
+  ['??', 'wh', '(', ')', '=', ';', '{', '}']
+end
+
+tokens = tokenizer(code)
+p tokens
+res = analyzer(tokens)
+
+res.each do |el|
+  p el
+end
